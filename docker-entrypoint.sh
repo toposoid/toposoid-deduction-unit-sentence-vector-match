576bfe378b681b8809430d82cb40a86454d48410
makoto-kubodera <makoto.kubodera@linked-ideal.com>
2024-03-15 09:53:43 +0900

    Upgrade to version 0.6-SNAPSHOT

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd /app/toposoid-deduction-unit-sentence-vector-match-web/target/universal
-toposoid-deduction-unit-sentence-vector-match-web-0.5/bin/toposoid-deduction-unit-sentence-vector-match-web
+toposoid-deduction-unit-sentence-vector-match-web-0.6-SNAPSHOT/bin/toposoid-deduction-unit-sentence-vector-match-web
